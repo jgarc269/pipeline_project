55402eafceb41e80cac5072156c3031494d77f73
Jesse Garcia <jgarc269@bolt.cs.ucr.edu>
2020-02-24 22:35:03 -0800

    broke

--- grading/driver_state.cpp
+++ grading/driver_state.cpp
@@ -47,16 +47,15 @@ void render(driver_state& state, render_type type)
     const data_geometry *dg[3];
     data_geometry temp[3];
     data_vertex in[3];
-    unsigned int k = 0;
-const auto first_vertex = state.vertex_data;
-    auto is_first_vertex = true;
+    
     switch(type)
     {	
         case render_type::triangle:
 	{ 
+		unsigned int k = 0;
 	    	for(int i = 0; i < state.num_vertices / triangle_edges; i++)
 	        {
-	            for(int j = 0; j < triangle_edges; k = k + state.floats_per_vertex)
+	            for(int j = 0; j < triangle_edges; k += state.floats_per_vertex)
 	            {
 			    in[j].data = &state.vertex_data[k];
 		            temp[j].data = in[j].data;
@@ -79,6 +78,8 @@ const auto first_vertex = state.vertex_data;
 	    case render_type::strip:
 	    break;
 
+	default:
+	    break;
     }
 }
 
@@ -103,11 +104,13 @@ void clip_triangle(driver_state& state, const data_geometry* in[3],int face)
 // fragments, calling the fragment shader, and z-buffering.
 void rasterize_triangle(driver_state& state, const data_geometry* in[3])
 {
+/*
     //std::cout<<"TODO: implement rasterization"<<std::endl;
     
     float a[3];
     float b[3];
     float c[3];
+
     float width_div;
     float height_div;
 
@@ -120,15 +123,16 @@ void rasterize_triangle(driver_state& state, const data_geometry* in[3])
     //and y from 0 to height. You will also need to account for the fact that the NDC (-1, -1) corresponds to the bottom left corner of the screen
     // but not the center of the bottom left pixel. Given (x, y) in NDC, what equation gives you (i, j) in pixel space 
     // (use w to denote width and h to denote height). 
-    vec3 vertices[3];
+   // vec3 vertices[3];
     for(int temp = 0; temp < 3; temp++)
     {
         float i = (width_div * ((*in)[temp].gl_Position[0]/(*in)[temp].gl_Position[3]) + (width_div - 0.5f));
         float j = (height_div * ((*in)[temp].gl_Position[1]/(*in)[temp].gl_Position[3]) + (height_div - 0.5f));
         float k = (width_div * ((*in)[temp].gl_Position[2]/(*in)[temp].gl_Position[3]) + (width_div - 0.5f));
-        vertices[temp][0] = i;
-        vertices[temp][1] = j;
-        vertices[temp][2] = k;
+       
+	a[temp] = i;
+        b[temp] = j;
+        c[temp] = k;
     } 
 	
 //    state.image_color[a[0], a[1], a[2]] = make_pixel(255,255,255);
@@ -151,11 +155,11 @@ void rasterize_triangle(driver_state& state, const data_geometry* in[3])
     {
 	min_b = 0;
     }
-    if(max_a > state.image_width + 1)
+    if(max_a > state.image_width)
     {
 	max_a = state.image_width;
     }
-    if(max_b > state.image_height + 1)
+    if(max_b > state.image_height)
     {
 	max_b = state.image_height;
     }
@@ -164,13 +168,10 @@ void rasterize_triangle(driver_state& state, const data_geometry* in[3])
     //AREA(abc) = 0.5 * ((bxcy − cxby ) − (axcy − cxay) + (axby − bxay))
     float area = (0.5f * ((a[1] * b[2] - a[2] * b[1]) - (a[0] * b[2] - a[2] * b[0]) + (a[0] * b[1] - a[1] * b[0])));
    
-
     //To rasterize the triangle, you can iterate over all pixels of the image. 
     //Say you are in the pixel with indices (i, j). 
     //You can use the barycentric coordinates of this pixel (i, j) to know if this pixel falls inside the triangle or not. 
     //Barycentric coordinates can be calculated using triangle areas. 
-
-/*
     for(int j = min_b; j < max_b; j++)
     {
 	for(int i = min_a; i < max_a; i++)
@@ -178,10 +179,10 @@ void rasterize_triangle(driver_state& state, const data_geometry* in[3])
 	    float alpha = (0.5f * ((a[1] * b[2] - a[2] * b[1]) + (a[1] - b[2]) * i + (a[2] - a[1]) * j)) / area;
             float beta =  (0.5f * ((a[2] * b[0] - a[0] * b[2]) + (a[2] - b[0]) * i + (a[0] - a[2]) * j)) / area;
             float gamma = (0.5f * ((a[0] * b[1] - a[1] * b[0]) + (a[0] - b[1]) * i + (a[1] - a[0]) * j)) / area;
-
+	   
+	   if(alpha >= 0 && beta >= 0 && gamma >= 0) 
 	    state.image_color[i + j * state.image_width] = make_pixel(255,255,255);
 	}
     }
 */
 }
-
